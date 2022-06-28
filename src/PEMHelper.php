f6fd7a8b58671a1a2772d816eb65b03ee2af2873
kovey <kovey@vip.qq.com>
2022-06-28 15:33:42 +0800

    add verify sign

--- src/PEMHelper.php
+++ src/PEMHelper.php
@@ -131,4 +131,20 @@ class PEMHelper
         }
         return $hash;
     }
+
+    public static function verifySignature($message, $signature, $address) 
+    {
+        $msglen = strlen($message);
+        $hash   = Keccak::hash("\x19Ethereum Signed Message:\n{$msglen}{$message}", 256);
+        $sign   = ["r" => substr($signature, 2, 64),
+                   "s" => substr($signature, 66, 64)];
+        $recid  = ord(hex2bin(substr($signature, 130, 2))) - 27;
+        if ($recid != ($recid & 1))
+            return false;
+
+        $ec = new EC('secp256k1');
+        $pubkey = $ec->recoverPubKey($hash, $sign, $recid);
+
+        return $address == self::pubKeyToAddress($pubkey);
+    }
 }
